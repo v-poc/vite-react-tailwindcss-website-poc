ed0abccf909fb898dbba626fa38602c66d051d37
NikoNi <bluepspower@163.com>
2026-04-14 23:24:24 +0800

    refactor: switch to vite-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
 import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  fmt: {},
+  lint: {"options":{"typeAware":true,"typeCheck":true}},
   plugins: [
     react(),
     viteMockServe({
